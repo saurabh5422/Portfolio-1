814ff44148b639783cd30c8ace509b7d3e4b7618
Saurabh Kumbhar <71523061+saurabh5422@users.noreply.github.com>
2021-08-12 13:47:45 +0530

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,423 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css "/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     <link rel="stylesheet" href="style.css">
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<!-- testmonial link  -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.pkgd.min.js"></script>
+
+    <title>Home</title>
+  </head>
+  <body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
+
+     <!-- Navbar  -->
+     <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+        <div class="container">
+          <a class="navbar-brand" href="#">PORTFOLIO</a>
+  
+          <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="mx-auto"></div>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#Home">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#About">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Services">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+         
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript">
+      var nav = document.querySelector('nav');
+
+      window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 100) {
+          nav.classList.add('bg-dark', 'shadow');
+        } else {
+          nav.classList.remove('bg-dark', 'shadow');
+        }
+      });
+    </script>
+ <!-- Portfolio top -->
+ <Section id="Home">
+      <div class="container-fluid Hcontent">
+        <div class="container  Hcontent">
+          <div class="row  justify-content-center headrow" >
+            
+        
+            <div class="col-sm-5 Hcontent">
+        <h1 class="HTitle">I'M BOB</h1>
+     <h2 class=" Hhead" style="font-weight: bold ;margin-bottom:0.5cm; color: rgb(185, 185, 185);">Application Developer</h2>
+        <p class="HDescription">I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences,
+           I am passionate about building excellent software that improves the lives of those around me.</p>
+           
+         <center> <a href="#Contact" class="btn Hire" role="button">Hire Me</a></center> 
+            </div>
+          
+            <div class="col-sm-5">
+              <img src="img/model.jpg"  class="img-fluid Himg">
+            </div>
+          </div>
+        </div>
+        </div>
+        </Section>
+      
+ <!--about section-->
+ <Section id="About">
+<div class="container-fluid ContAbout">
+  <div class="container">
+  <h1 class="AbtHead">ABOUT ME </h1>
+  <div class= "row AbtRow">
+
+    
+
+    <div class="col-sm-6 colimg">
+<img src="img/model.jpg" class="img-fluid imgg" > 
+    </div>
+
+    <div class="col-sm-6 coltext ">
+<p class="AbtTittle">Who i am?</p>
+<p class="AbtContent">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+   It has survived not only five centuries,</p>
+
+   <a href="" class="btn Download" role="button">Download CV</a> 
+    </div>
+    
+</div>
+</div>
+</div>
+
+</Section>
+ <!--Services section-->
+ <Section id="Services">
+ <div class="container">
+
+  <h1 class="ServHead">SERVICES OFFERED </h1>
+<center>
+  <div class="row">
+      <div class="col-md-3 col-sm-6">
+          <div class="serviceBox">
+            
+              <div class="service-icon">
+                  <span><i class="fa fa-globe"></i></span>
+              </div>
+          
+              <h3 class="title">Web Design</h3>
+              <p class="description">Lorem ipsum dolor sit amet conse ctetur adipisicing elit. Qui quaerat fugit quas veniam perferendis repudiandae sequi, dolore quisquam illum.</p>
+              <a href="#" class="read-more">Read More</a>
+          </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+          <div class="serviceBox cyan">
+              <div class="service-icon">
+                  <span><i class="fa fa-rocket"></i></span>
+              </div>
+              <h3 class="title">Web Development</h3>
+              <p class="description">Lorem ipsum dolor sit amet conse ctetur adipisicing elit. Qui quaerat fugit quas veniam perferendis repudiandae sequi, dolore quisquam illum.</p>
+              <a href="#" class="read-more">Read More</a>
+          </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="serviceBox cyan">
+            <div class="service-icon">
+                <span><i class="fa fa-cloud"></i></span>
+            </div>
+            <h3 class="title">App Development</h3>
+            <p class="description">Lorem ipsum dolor sit amet conse ctetur adipisicing elit. Qui quaerat fugit quas veniam perferendis repudiandae sequi, dolore quisquam illum.</p>
+            <a href="#" class="read-more">Read More</a>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="serviceBox cyan">
+          <div class="service-icon">
+              <span><i class="fa fa-desktop"></i></span>
+          </div>
+          <h3 class="title">UI/UX design </h3>
+          <p class="description">Lorem ipsum dolor sit amet conse ctetur adipisicing elit. Qui quaerat fugit quas veniam perferendis repudiandae sequi, dolore quisquam illum.</p>
+          <a href="#" class="read-more">Read More</a>
+      </div>
+  </div>
+  </div>
+</center>
+</div>
+ </Section>
+
+<!-- teams section -->
+
+<div class="container">
+  <h1 class="TeamHead">MY TEAM </h1>
+  <div class="row d-flex justify-content-center">
+      <div class="col-md-4 col-sm-6">
+          <div class="our-team">
+              <img src="img/model1.jpg" alt="">
+              <ul class="social">
+                  <li><a href="#" class="fa fa-facebook"></a></li>
+                  <li><a href="#" class="fa fa-instagram"></a></li>
+                  <li><a href="#" class="fa fa-google-plus"></a></li>
+                  <li><a href="#" class="fa fa-linkedin"></a></li>
+                  <li><a href="#" class="fa fa-twitter"></a></li>
+              </ul>
+              <div class="team-content">
+                  <h3 class="title">Williamson</h3>
+                  <span class="post">Web Developer</span>
+              </div>
+          </div>
+      </div>
+      <div class="col-md-4 col-sm-6">
+          <div class="our-team">
+              <img src="img/model2.jpg" alt="">
+              <ul class="social">
+                  <li><a href="#" class="fa fa-facebook"></a></li>
+                  <li><a href="#" class="fa fa-instagram"></a></li>
+                  <li><a href="#" class="fa fa-google-plus"></a></li>
+                  <li><a href="#" class="fa fa-linkedin"></a></li>
+                  <li><a href="#" class="fa fa-twitter"></a></li>
+              </ul>
+              <div class="team-content">
+                  <h3 class="title">Kristiana</h3>
+                  <span class="post">Web Designer</span>
+              </div>
+          </div>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <div class="our-team">
+            <img src="img/model3.jpg" alt="">
+            <ul class="social">
+                <li><a href="#" class="fa fa-facebook"></a></li>
+                <li><a href="#" class="fa fa-instagram"></a></li>
+                <li><a href="#" class="fa fa-google-plus"></a></li>
+                <li><a href="#" class="fa fa-linkedin"></a></li>
+                <li><a href="#" class="fa fa-twitter"></a></li>
+            </ul>
+            <div class="team-content">
+                <h3 class="title">Kristiana</h3>
+                <span class="post">Web Designer</span>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+  <!-- Testimonial slider -->
+
+  <div class="container cTest">
+    <h1 class="TestHead">WHAT CLIENTS SAY</h1>
+<div class="main-gallery">
+    <div class="gallery-cell">
+      <div class="testimonial">
+          <img class="testimonial-avatar" src="img/model3.jpg">
+        <q class="testimonial-quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mauris ex, gravida ut leo eu, rhoncus porta orci. Fusce vitae rutrum nulla."</q>
+        <span class="testimonial-author">Joe Smith, CEO of Cubix</span>
+      </div>
+    </div>
+    <div class="gallery-cell">
+       <div class="testimonial">
+          <img class="testimonial-avatar" src="img/model2.jpg">
+        <q class="testimonial-quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mauris ex, gravida ut leo eu, rhoncus porta orci. Fusce vitae rutrum nulla."</q>
+        <span class="testimonial-author">Lisa Jones, Freelance Web Developer</span>
+      </div>
+    </div>
+    <div class="gallery-cell">
+          <div class="testimonial">
+          <img class="testimonial-avatar" src="img/model1.jpg">
+        <q class="testimonial-quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mauris ex, gravida ut leo eu, rhoncus porta orci. Fusce vitae rutrum nulla."</q>
+        <span class="testimonial-author">Ryan Waltz, Front-End Developer</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <script>
+      var flkty = new Flickity( '.main-gallery', {
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  prevNextButtons: false,
+  autoPlay: 5000
+});
+  </script>
+
+
+  <!-- contact -->
+  <Section id="Contact">
+   <div class="formthree ptb-100">
+     <div class="contHead">CONTACT US</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8  mx-auto ">
+
+              <!-- PHP code  -->
+              <?php
+              
+              // if user click on send button
+
+              if(isset($_POST['send'])){
+                // getting all user entered data
+
+                $name = $_POST['name'];
+                $recipent = $_POST['email'];
+                $number = $_POST['number'];
+                $message = $_POST['message'];
+                $sender ="From: saurabhkumbhar0208@gmail.com";
+
+                // if user leave field empty
+                if(empty($name) || empty($recipent) || empty($number) || empty($message)){
+                  ?>
+                  <div class="alert alert-danger text-center">
+                    <?php echo "All Input fields are required" ?>
+                </div>
+                  <?php
+                }else{
+
+                  // php function to send mail
+
+                  if(mail($name, $email, $name, $number, $sender)){
+                  ?>
+                  <!-- success message -->
+
+                  <div class="alert alert-success text-center">
+
+                  <?php echo "your mail has sent to $recipent " ?>
+                  </div>
+                  <?php
+                  }
+
+                }
+
+              }
+
+              ?>
+
+                <form action="Home.php" method="POST" autocomplete="off">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group"> <label class="sr-only">Name</label> <input type="text" name="name" class="form-control" required="" id="nameSix" placeholder="Your Name"> </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group"> <label class="sr-only">Email</label> <input type="email"  name="recipent" class="form-control" required="" id="emailSix" placeholder="Email Address"> </div>
+                        </div>
+                        <br>
+                        <div class="col-md-12">
+                          <div class="form-group"> <label class="sr-only">Contact</label> <input type="number"  name="number" class="form-control" required="" id="contactSix" placeholder="Contact"> </div>
+                      </div>
+                      
+                    </div>
+                    
+                    <div class="form-group"> <label class="sr-only">Message</label> <textarea class="form-control" name="message" required="" rows="7" placeholder="Write Message"></textarea> </div> 
+                    <button type="submit" name="send" value="Submit" class="btn btn-primary">Send Message</button>
+                </form>
+            </div>
+        </div> 
+    </div>
+</div>
+
+</Section>
+
+<!-- footer -->
+
+<footer class="text-center text-white" style="background-color: #000000;">
+    <!-- Grid container -->
+    <div class="container pt-4">
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Facebook -->
+        <a
+          class="btn btn-link btn-floating btn-lg text-white m-1"
+          href="#!"
+          role="button"
+          data-mdb-ripple-color="dark"
+          ><i class="fa fa-facebook-f"></i
+        ></a>
+  
+        <!-- Twitter -->
+        <a
+          class="btn btn-link btn-floating btn-lg text-white m-1"
+          href="#!"
+          role="button"
+          data-mdb-ripple-color="dark"
+          ><i class="fa fa-twitter"></i
+        ></a>
+  
+        
+        
+  
+        <!-- Instagram -->
+        <a
+          class="btn btn-link btn-floating btn-lg text-white m-1"
+          href="#!"
+          role="button"
+          data-mdb-ripple-color="dark"
+          ><i class="fa fa-instagram"></i
+        ></a>
+  
+        <!-- Linkedin -->
+        <a
+          class="btn btn-link btn-floating btn-lg text-white m-1"
+          href="#!"
+          role="button"
+          data-mdb-ripple-color="dark"
+          ><i class="fa fa-linkedin"></i
+        ></a>
+        <!-- Github -->
+        <a
+          class="btn btn-link btn-floating btn-lg text-white m-1"
+          href="#!"
+          role="button"
+          data-mdb-ripple-color="dark"
+          ><i class="fa fa-github"></i
+        ></a>
+      </section>
+      <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+  
+    <!-- Copyright -->
+    <div class=" copyright text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2021 Copyright:
+      <a class="copyright" href="">SKAITAS</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+
+ </body>
+</html>
